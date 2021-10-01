7876e1a043e1f101f9a2af2af31963bbcbaa50f0
AugustoSavi <guto_savi@outlook.com>
2021-09-30 23:31:38 -0300

    iniciando exclusão

--- src/main/java/com/example/core/Servlet/CandidatoServlet.java
+++ src/main/java/com/example/core/Servlet/CandidatoServlet.java
@@ -1,6 +1,7 @@
 package com.example.core.Servlet;
 
 import com.example.core.Model.Candidato;
+import com.example.core.View.CandidatosListCreator;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,14 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.io.PrintWriter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @WebServlet(name = "CandidatoServlet", value = "/candidato")
 public class CandidatoServlet extends HttpServlet {
 
     private List<Candidato> candidatos = new ArrayList<>();
+
+    private CandidatosListCreator candidatosListCreator = new CandidatosListCreator();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+        PrintWriter out = response.getWriter();
+        out.println(candidatosListCreator.getTableHtml(candidatos));
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -27,6 +38,9 @@ public class CandidatoServlet extends HttpServlet {
         String id = request.getParameter("id");
         String nome = request.getParameter("nome");
         String numero = request.getParameter("numero");
+
+        System.out.println("doPost: "+ id);
+
         if (Objects.isNull(id)){
             id = UUID.randomUUID().toString();
             Candidato candidato = new Candidato(id,nome,Integer.valueOf(numero));
@@ -41,6 +55,24 @@ public class CandidatoServlet extends HttpServlet {
                 }
             }
         }
-        response.sendRedirect("candidato.html");
+        PrintWriter out = response.getWriter();
+        out.println(candidatosListCreator.getTableHtml(candidatos));
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("id");
+
+        System.out.println("doDelete: "+ id);
+
+        candidatos = candidatos.stream().filter(candidato -> !candidato.getId().equals(id)).collect(Collectors.toList());
+
+        out.println(candidatosListCreator.getTableHtml(candidatos));
     }
 }
--- src/main/java/com/example/core/View/CandidatosListCreator.java
+++ src/main/java/com/example/core/View/CandidatosListCreator.java
@@ -6,60 +6,63 @@ import java.util.List;
 
 public class CandidatosListCreator {
 
-//    public String getTableHtml(List<Candidato> candidatos){
-//        String table = "<html>
-//                    <head>
-//                        <meta charset="utf-8">
-//                        <title>Página de Categorias</title>
-//                        <style>
-//                            table, th, td {
-//                              border: 1px solid black;
-//                            }
-//                            th, td {
-//                              text-align:center;
-//                            }
-//                            th {
-//                                background-color: #149ce6;
-//                                color: white;
-//                            }
-//                        </style>
-//                    </head>
-//                <body>
-//                    <h1>Categorias da plataforma Sunty</h1>
-//                    <table>
-//                        <tr>
-//                        <th>Nome</th>
-//                        <th>Descrição</th>
-//                        <th>Ícone</th>
-//                        <th>Cor</th>
-//                        <th>Número de cursos</th>
-//                        <th>Total de horas dos cursos</th>
-//                        <th>Subcategorias</th>
-//                        </tr>
-//                "
-//        return table;
-//
-//        for (Candidato candidato : candidatos) {
-//            printStream.println("<tr>" +
-//                    "<td>" + category.getName() + "</td>" +
-//                    "<td>" + category.getShortDescription() + "</td>" +
-//                    "<td>" + category.getPathImg() + "</td>" +
-//                    "<td>" + category.getHexHtmlColor() + "</td>" +
-//                    "<td>" + category.getCoursesQuantity() + "</td>" +
-//                    "<td>" + category.getTotalTimeToFinishAnHours() + "</td>" +
-//                    "<td>");
-//            for (SubCategory subCategory : category.getSubCategoryList()) {
-//                printStream.print(subCategory.getName() + "<br><br>");
-//                printStream.println(subCategory.getShortDescription());
-//                printStream.println("<br><hr>");
-//            }
-//            printStream.println("</td>");
-//            printStream.println("</tr>");
-//        }
-//        printStream.println("</table>");
-//        printStream.println("</body>");
-//        printStream.println("</html>");
-//    }
-//}
-//    }
+    public String getTableHtml(List<Candidato> candidatos){
+        String page = "<html>" +
+                "<head> " +
+                "<meta charset=\"UTF-8\"> " +
+                "<title>Candidatos</title> <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU\" crossorigin=\"anonymous\">" +
+                "<script src=\"https://kit.fontawesome.com/311492eabd.js\" crossorigin=\"anonymous\"></script>" +
+                "</head>" +
+                "<body class=\"container text-align\">" +
+                    "<div id=\"button-cadastrar\">" +
+                        "<a href=\"newCandidato.html\" class=\"btn btn-primary btn-block mt-5\">Cadastrar candidato</a>" +
+                    "</div>" +
+
+                "<div id=\"table-dados\" class=\"mt-3\">" +
+                    "<table class=\"table\">" +
+                        "<thead>" +
+                        "<tr>" +
+                            "<th scope=\"col\">Id</th>" +
+                            "<th scope=\"col\">Nome</th>" +
+                            "<th scope=\"col\">Numero Candidato</th>" +
+                            "<th scope=\"col\"></th>" +
+                            "<th scope=\"col\"></th>" +
+                        "</tr>" +
+                        "</thead>" +
+                "<tbody>";
+
+        String dados = "";
+        for (Candidato candidato : candidatos) {
+            dados =  "<tr>" +
+                        "<td>" + candidato.getId() + "</td>" +
+                        "<td>" + candidato.getNome() + "</td>" +
+                        "<td>" + candidato.getNumeroCandidato() + "</td>" +
+                        "<td><a onclick=_delete(\""+ candidato.getId() +"\") class=\"fas fa-pencil-alt\"></a></td>" +
+                    "</tr>" ;
+        }
+        page += dados;
+        page += "</tbody>";
+        page += "</table>";
+        page += "</div>";
+        page += "</body>";
+        page +="<script>\n" +
+                "    function _delete(id){\n" +
+                "         console.log(id); \n" +
+                "        var HEADERS = new Headers();\n" +
+                "        HEADERS.append('id', id);\n" +
+                "        var CONFIG = { method: 'DELETE',\n" +
+                "            headers: HEADERS,\n" +
+                "            mode: 'cors'};\n" +
+                "\n" +
+                "        fetch('candidato?'+ new URLSearchParams({id}),CONFIG)" +
+                "       .then(function(response) {" +
+                "       if (response.ok) {" +
+                "       console.log(\"request ok\")} " +
+                "       else {console.log(\"error: \" + response);}})" +
+                "    }\n" +
+                "</script>";
+        page += "</html>";
+
+        return page;
+    }
 }
